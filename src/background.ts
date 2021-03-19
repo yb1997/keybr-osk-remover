420b912474e52eab259fa7dcb5555354fc2ac6e2
yb1997 <ybisht1997@gmail.com>
2021-03-20 00:05:49 +0530

    disable extension for other sites

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,15 @@
 // This file is ran as a background script
-console.log("Hello from background script!")
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { hostEquals: "www.keybr.com" },
+                    }),
+                ],
+                actions: [new chrome.declarativeContent.ShowPageAction()],
+            },
+        ]);
+    });
+});
